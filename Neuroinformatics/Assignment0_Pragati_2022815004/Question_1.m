d5cd37c5b92d79fe36eb4b94f0dc1301dd1c9c8b
pragati-neuroinfomatics <pragati.gupta@research.iiit.ac.in>
2024-01-09 20:06:08 +0530

    Version1

--- Neuroinformatics/Assignment0_Pragati_2022815004/Question_1.m
+++ Neuroinformatics/Assignment0_Pragati_2022815004/Question_1.m
@@ -15,8 +15,27 @@ condition2 = transposePower_mat(:, 2);
 
 % Perform independent t-test
 [h, p, ci, stats] = ttest2(condition1, condition2);
-%The p-value is 0.0605. 
+disp = stats
+%The p-value is 0.0605 and  h= 0 whichindicates that 
+% ttest2 does not reject the null hypothesis at the default 5% significance level.
 
 % Question 2 
-Fs= 2;
-a = sin(2*pi*60*t) 
+t= [0:0.001:4]; %(Time-period);
+f = [2 8 12 25]; %(Frequrencies in Hz)
+A = [0.5 1 1.5 2];
+phase = [pi/1 pi/2 pi/3 pi/4];
+figure;
+for i = 1:4 
+    x(i,:) = A(i)*sin(2*pi*f(i)*t+phase(i));
+    subplot (5,1,i)
+    plot (x(i,:))
+    xlim([0 4000])
+end 
+y = mean(x,1);
+subplot (5,1,5)
+plot (y)
+xlim([0 4000])
+
+
+
+
